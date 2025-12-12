6e011c3bfecb20057d80b8196979ab370b1edad3
jed <166913797+jims1001@users.noreply.github.com>
2025-12-12 15:39:11 +0400

    创建工程

---
+++ src/main/java/com/PPCloud/PP_Platform_Service/PpPlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.PPCloud.PP_Platform_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpPlatformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpPlatformServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PPCloud/PP_Platform_Service/PpPlatformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PPCloud.PP_Platform_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpPlatformServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
